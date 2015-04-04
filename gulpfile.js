b74bc7b8b4ef840b45584c96633fcdb87337d265
Jonathan Wilde <hello@jwilde.me>
2015-04-03 23:33:19 -0400

    Actually add the styles into the build.

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,20 @@ var gulp = require('gulp');
 var webpack = require('gulp-webpack');
 var webpackConfig = require('./example/webpack.config');
 
-gulp.task('build-node', function() {
+gulp.task('build-node-css', function() {
+  return gulp.src('src/**/*.css')
+    .pipe(gulp.dest('lib/'));
+});
+
+gulp.task('build-node-js', function() {
   return gulp.src('src/**/*.js')
     .pipe(babel({
       experimental: true
     }))
     .pipe(gulp.dest('lib/'));
 });
+
+gulp.task('build-node', ['build-node-js', 'build-node-css']);
 
 gulp.task('build-examples', function() {
   return gulp.src('example/')
