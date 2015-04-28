026f00b97faa22af4abde98df2df10f0ac605389
Jonathan Wilde <hello@jwilde.me>
2015-04-28 15:59:28 -0400

    Add theoretically failing test.

---
+++ src/helpers/__tests__/getDays-test.js
@@ -0,0 +1,13 @@
+var expect = require('expect');
+var getDays = require('../getDays');
+var moment = require('moment');
+
+describe('getDays', () => {
+  it('should align weeks with the correct start days of week', () => {
+    var en = moment('2015-04-10').locale('en');
+    expect(getDays(en, en)[0][0].value).toEqual('29');
+
+    var fr = moment('2015-04-10').locale('en');
+    expect(getDays(fr, fr)[0][0].value).toEqual('30');
+  });
+});
